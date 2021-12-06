fbd3862a862a1112d123b865ce8c522e9a31bd58
Marianne_Rario <mariannecastillorario27@gmail.com>
2021-12-06 19:09:13 +0800

    changed local db to remote

--- connection/connect.php
+++ connection/connect.php
@@ -1,16 +1,16 @@
 <?php
 
     // DEVELOPMENT
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "project";
+    // $server = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $db_name = "project";
 
     // REMOTE
-    // $server = "remotemysql.com";
-    // $username = "DvCvy5s93e";
-    // $password = "R1PBw2P1VI";
-    // $db_name = "DvCvy5s93e";
+    $server = "remotemysql.com";
+    $username = "DvCvy5s93e";
+    $password = "R1PBw2P1VI";
+    $db_name = "DvCvy5s93e";
 
     // 
     $errors = array();
